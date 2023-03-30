0636cb5fff1f29146da6ec2752fa6367553e4b05
Cristhiam Gomez <cristhiam.gomez@clouxter.com>
2023-03-30 15:01:09 -0500

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function handler(event) {
     console.log("Job Stared" )
 
     return {
-        body: JSON.stringify({message: 'SUCCESS'}),
+        body: JSON.stringify({message: 'SUCCESS2'}),
         statusCode: 200,
     };
 }
